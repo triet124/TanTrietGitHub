90aabe9713588803fbb0b2a009ba15dfc387fb6d
TrietTT <trantantriet1811@gmail.com>
2024-05-29 08:32:24 +0700

    Layout
    Giao diện tách layout

---
+++ Layout/Header/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Header.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
